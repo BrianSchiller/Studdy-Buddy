0ed9628ec113a290a1f737866fab09795cf2ce04
RandomMonk3y <randommonkeyking@gmail.com>
2024-12-04 18:11:12 +0100

    Added exam model and views

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, UserProgress, Topic, Word, Mistake, ExperimentGroup
+from .models import User, UserProgress, Topic, Word, Mistake, ExperimentGroup, Exam, ExamAnswer, ExamResult
 
 admin.site.register(User)
 admin.site.register(UserProgress)
@@ -7,5 +7,8 @@ admin.site.register(Topic)
 admin.site.register(Word)
 admin.site.register(Mistake)
 admin.site.register(ExperimentGroup)
+admin.site.register(Exam)
+admin.site.register(ExamAnswer)
+admin.site.register(ExamResult)
 
 # Register your models here.
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import check_user_exists, update_user_progress, get_random_words, WordListView, UserProgressListView, TopicListView
+from .views import check_user_exists, update_user_progress, get_random_words, WordListView, UserProgressListView, TopicListView, get_exam, submit_exam
 
 
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
     path('words/<int:topic_id>/', WordListView.as_view(), name='word_list'),
     path('topics/', TopicListView.as_view(), name='topic_list'),
     path('random_words/<int:amount>/', get_random_words, name='random_words'),
+    path('exam/<int:topic_id>/', get_exam, name='get_exam'),
+    path('submit_exam/<int:exam_id>/<str:username>/', submit_exam, name='submit_exam'),
 ]
