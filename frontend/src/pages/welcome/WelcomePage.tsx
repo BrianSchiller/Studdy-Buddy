8f9b8476d6cad366d22fb5dbe3a1f02a8483ff1d
RandomMonk3y <randommonkeyking@gmail.com>
2024-12-11 17:52:33 +0100

    Fixed some unresponsive frontend stuff

--- frontend/src/pages/welcome/WelcomePage.tsx
+++ frontend/src/pages/welcome/WelcomePage.tsx
@@ -17,7 +17,7 @@ const LearningCard = styled.div`
     padding: 15px;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    margin-bottom: 20px;
+    
 `;
 
 const FlagImage = styled.img`
