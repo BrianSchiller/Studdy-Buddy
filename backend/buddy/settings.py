4bebb446d519abc344512fe6b68f44b4068eccd9
RandomMonk3y <randommonkeyking@gmail.com>
2024-12-12 12:26:21 +0100

    Please media

--- backend/buddy/settings.py
+++ backend/buddy/settings.py
@@ -128,12 +128,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'buddy/static'),  # Add this line to serve images from the buddy/images folder
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'buddy/media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'buddy/images'),  # Add this line to serve images from the buddy/images folder
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
