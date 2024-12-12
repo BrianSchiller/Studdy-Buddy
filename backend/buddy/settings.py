0fd22d6d10739252ebe926438a7eea9f7beaad92
RandomMonk3y <randommonkeyking@gmail.com>
2024-12-12 12:32:43 +0100

    pls

--- backend/buddy/settings.py
+++ backend/buddy/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'buddy/static'),  # Add this line to serve images from the buddy/images folder
-]
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'buddy/media')
