f44085226d641be4e09849b21fb6774c28509ec6
RandomMonk3y <randommonkeyking@gmail.com>
2024-12-14 12:29:39 +0100

    Removed image from word serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 class WordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Word
-        fields = ['id', 'spanish', 'english', 'image', 'sentence']
+        fields = ['id', 'spanish', 'english', 'sentence']
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
