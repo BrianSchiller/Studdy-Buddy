3304e2e09fe22c5c8e3077aea527181451c39546
Masuthan Mathiyalakan <masuthan.mathiyalakan@ruhr-uni-bochum.de>
2024-12-23 16:10:07 +0100

    change quiz wordpair flag

--- frontend/src/pages/wordpair_quiz/WordPairQuizPage.tsx
+++ frontend/src/pages/wordpair_quiz/WordPairQuizPage.tsx
@@ -149,55 +149,59 @@ const WordPairQuizPage: React.FC = () => {
                     timer={timer}
                 />
                 <WordPairRow>
-    <WordGrid isPicturePhase={isPicturePhase}>
-        <FlagHeader>
-            <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"
-                alt="English Flag"
-            />
-            English
-        </FlagHeader>
-        {shuffledEnglish.map((word) =>
-            isPicturePhase ? (
-                <ImageCard
-                    key={`image-${word.id}`}
-                    selected={selectedEnglish === word.id}
-                    shake={shake}
-                    imageUrl={word.picture}
-                    onClick={() => handleWordClick(word.id, "english")}
-                />
-            ) : (
-                <WordCard
-                    key={`english-${word.id}`}
-                    selected={selectedEnglish === word.id}
-                    shake={shake}
-                    onClick={() => handleWordClick(word.id, "english")}
-                >
-                    {word.english}
-                </WordCard>
-            )
-        )}
-    </WordGrid>
-    <WordGrid>
-        <FlagHeader>
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png"
-                alt="Spanish Flag"
-            />
-            Spanish
-        </FlagHeader>
-        {shuffledSpanish.map((word) => (
-            <WordCard
-                key={`spanish-${word.id}`}
-                selected={selectedSpanish === word.id}
-                shake={shake}
-                onClick={() => handleWordClick(word.id, "spanish")}
-            >
-                {word.spanish}
-            </WordCard>
-        ))}
-    </WordGrid>
-</WordPairRow>
+                    <WordGrid isPicturePhase={isPicturePhase}>
+                        <FlagHeader>
+                            <img
+                                src={
+                                    isPicturePhase
+                                        ? "https://cdn-icons-png.flaticon.com/512/860/860813.png" // Camera icon
+                                        : "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png" // English flag
+                                }
+                                alt={isPicturePhase ? "Camera Icon" : "English Flag"}
+                            />
+                            {isPicturePhase ? "Pictures" : "English"}
+                        </FlagHeader>
+                        {shuffledEnglish.map((word) =>
+                            isPicturePhase ? (
+                                <ImageCard
+                                    key={`image-${word.id}`}
+                                    selected={selectedEnglish === word.id}
+                                    shake={shake}
+                                    imageUrl={word.picture}
+                                    onClick={() => handleWordClick(word.id, "english")}
+                                />
+                            ) : (
+                                <WordCard
+                                    key={`english-${word.id}`}
+                                    selected={selectedEnglish === word.id}
+                                    shake={shake}
+                                    onClick={() => handleWordClick(word.id, "english")}
+                                >
+                                    {word.english}
+                                </WordCard>
+                            )
+                        )}
+                    </WordGrid>
+                    <WordGrid>
+                        <FlagHeader>
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png"
+                                alt="Spanish Flag"
+                            />
+                            Spanish
+                        </FlagHeader>
+                        {shuffledSpanish.map((word) => (
+                            <WordCard
+                                key={`spanish-${word.id}`}
+                                selected={selectedSpanish === word.id}
+                                shake={shake}
+                                onClick={() => handleWordClick(word.id, "spanish")}
+                            >
+                                {word.spanish}
+                            </WordCard>
+                        ))}
+                    </WordGrid>
+                </WordPairRow>
             </DashboardPanel>
         </StyledContainer>
     );
