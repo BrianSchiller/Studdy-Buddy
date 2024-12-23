e1dacead77998c668b3da9fe8c2fd419dc110b38
Masuthan Mathiyalakan <masuthan.mathiyalakan@ruhr-uni-bochum.de>
2024-12-23 14:58:01 +0100

    hello

--- frontend/src/pages/wordpair_quiz/stylesWordPairQuiz.ts
+++ frontend/src/pages/wordpair_quiz/stylesWordPairQuiz.ts
@@ -85,27 +85,6 @@ const shakeAnimation = keyframes`
   100% { transform: translateX(0); }
 `;
 
-export const WordGridLeft = styled.div<{ isPicturePhase: boolean }>`
-    display: ${({ isPicturePhase }) => (isPicturePhase ? "grid" : "flex")};
-    grid-template-columns: ${({ isPicturePhase }) =>
-        isPicturePhase ? "repeat(2, 1fr)" : "none"};
-    gap: ${({ isPicturePhase }) => (isPicturePhase ? "20px" : "15px")};
-    width: 45%;
-
-    @media (max-width: 768px) {
-        width: 100%;
-        grid-template-columns: ${({ isPicturePhase }) =>
-            isPicturePhase ? "repeat(2, 1fr)" : "none"};
-        gap: 15px;
-    }
-
-    @media (max-width: 480px) {
-        grid-template-columns: ${({ isPicturePhase }) =>
-            isPicturePhase ? "repeat(2, 1fr)" : "none"};
-        gap: 10px;
-    }
-`;
-
 // WordCard mit Shake-Animation
 export const WordCard = styled.div<{ selected?: boolean; shake?: boolean }>`
     padding: 15px;
@@ -166,7 +145,6 @@ export const ImageCard = styled.div<{ selected?: boolean; shake?: boolean; image
         height: 80px;
     }
 `;
-
 
 export const FlagHeader = styled.div`
     display: flex;
